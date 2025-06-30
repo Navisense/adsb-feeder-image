cebc0f90610b01fbafd7bca04299b2a8bd8b245f
Marc Lehmann <marc@navisense.de>
2025-06-30 16:35:07 +0200

    Expand GPT partition tables
    With GPT partition tables, parted offers to expand the partition for us,
    which we can just accept using --fix in a print command. Not doing this
    will not work, because piping the "yes" into it is brittle enough that
    it breaks.

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/expand-rootfs.bash
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/expand-rootfs.bash
@@ -8,6 +8,13 @@ DEVICE=/dev/mmcblk0
 ROOT_PARTITION_NUMBER=2
 ROOT_PARTITION_DEVICE=/dev/mmcblk0p2
 
+# First, just print the partition table, because for gpt table parted actually
+# offers to maximize the last partition, which we can accept via --fix.
+parted --script --fix ${DEVICE} print
+if [ $? -ne 0 ] ; then
+    log_and_exit_sync 1 $0 "Failed to print the partition table."
+fi
+
 echo yes | parted ---pretend-input-tty ${DEVICE} resizepart ${ROOT_PARTITION_NUMBER} 100%
 if [ $? -ne 0 ] ; then
     log_and_exit_sync 1 $0 "Failed to resize root partition."
