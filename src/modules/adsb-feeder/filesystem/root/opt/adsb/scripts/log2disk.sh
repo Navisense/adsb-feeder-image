a1af0f0138a02d67b43a6f47baba6eb1a3660f61
Marc Lehmann <marc@navisense.de>
2025-09-23 09:56:37 +0200

    Refactor mem-log rotation script

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/log2disk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -f /run/porttracker-feeder.log ]]; then
-    TIMESTAMP=$(date +%Y-%m-%d+%H:%M:%S)
-    mkdir -p /opt/adsb/logs
-    zstd /run/porttracker-feeder.log -o /opt/adsb/logs/adsb-setup.log."$TIMESTAMP".zst
-    truncate -s 0 /run/porttracker-feeder.log
-    find /opt/adsb/logs -name adsb-setup.log.\* -ctime +7 | xargs rm -f
-fi
---
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/rotate-ramlog-to-file.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Rotate the in-memory log file to disk.
+#
+# Append the contents of the in-memory log file in /run to the permanent log
+# file in /var/log. The optional first argument gives a minimum number of lines
+# in the log file to do this (to reduce memory writes if there are only a few
+# lines).
+
+if [ ! -f /opt/adsb/scripts/lib-common.bash ] ; then
+    echo "Missing /opt/adsb/scripts/lib-common.bash, unable to continue."
+    exit 1
+fi
+source /opt/adsb/scripts/lib-common.bash
+
+min_lines_to_do_rotation=$1
+
+if [ ! -f "${LOG_FILE}" ] ; then
+    echo "Log file doesn't exist, nothing to do."
+    exit 0
+fi
+
+if [ -n $min_lines_to_do_rotation ] \
+        && (( $(wc -l < "${LOG_FILE}") < "${min_lines_to_do_rotation}" )); then
+    echo "Not enough lines to rotate the log file."
+    exit 0
+fi
+
+cat "${LOG_FILE}" >> /var/log/porttracker-feeder.log
+truncate -s 0 "${LOG_FILE}"
