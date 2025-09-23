d85dc79b934791ee921afb2fdafe941b5820e3a4
Marc Lehmann <marc@navisense.de>
2025-09-23 10:33:31 +0200

    Add update timers
    Add separate update services and timers for OS and feeder. These can be
    enabled and just do the update, rather than having a script check the
    app config from the outside.

---
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/update-feeder-to-latest-stable.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update the Porttracker feeder to the latest stable version.
+
+if [ ! -f /opt/adsb/scripts/lib-common.bash ] ; then
+    echo "Missing /opt/adsb/scripts/lib-common.bash, unable to continue."
+    exit 1
+else
+    . /opt/adsb/scripts/lib-common.bash
+    . /opt/adsb/scripts/lib-install.bash
+    rootcheck
+fi
+
+# First check if there is even an update available.
+current_version=$(/opt/adsb/adsb-setup/config.py get base_version)
+latest_version=$(find_latest_stable_version)
+if [ "${current_version}" = "${latest_version}" ] ; then
+    log $0 "No new feeder version available."
+else
+    log $0 "Starting nightly feeder update from ${current_version} " \
+        "to ${latest_version}."
+    systemd-run -u nightly-adsb-feeder-update \
+        /opt/adsb/scripts/update-feeder.bash $latest_version
+    log $0 "Nightly feeder update done."
+fi
