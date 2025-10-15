5a1c101a3ab8c4d1c3add7691c51ecbc726e91a7
Marc Lehmann <marc@navisense.de>
2025-10-15 10:53:20 +0200

    Disable refresh button
    ... if the last update was very recently.

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/static/js/update-time-counter.js
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/static/js/update-time-counter.js
@@ -2,10 +2,16 @@
 
 /**
  * Utility that updates an element's text with the time since some timestamp.
+ *
+ * Takes an ID for a counter element, and one for a refresh button. Updates the
+ * counter element once per second with a human-readable duration since the last
+ * call to refresh() or to construction. During the first few seconds, that
+ * string is "just now", and during this time the button is disabled.
  */
 class UpdateTimeCounter {
-  constructor(elementId) {
-    this.elementId = elementId;
+  constructor(counterElementId, buttonElementId) {
+    this.counterElementId = counterElementId;
+    this.buttonElementId = buttonElementId;
     this.updateTs = Date.now();
     this.intervalId = null;
     this.reset();
@@ -23,16 +29,21 @@ class UpdateTimeCounter {
   update() {
     let seconds = Math.round((new Date() - this.updateTs) / 1000);
     if (seconds <= 3) {
-      $(`#${this.elementId}`).text("just now");
-    } else if (seconds < 60) {
-      $(`#${this.elementId}`).text(`${seconds} seconds ago`);
+      $(`#${this.buttonElementId}`).attr("disabled", "");
+      $(`#${this.counterElementId}`).text("just now");
     } else {
-      const minutes = Math.floor(seconds / 60);
-      seconds = seconds % 60;
-      let s = "s";
-      if (minutes == 1) s = "";
-      $(`#${this.elementId}`).text(
-        `${minutes} minute${s}, ${seconds} seconds ago`);
+      $(`#${this.buttonElementId}`).removeAttr("disabled");
+      if (seconds < 60) {
+        $(`#${this.counterElementId}`).text(`${seconds} seconds ago`);
+      } else {
+        const minutes = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        let s = "s";
+        if (minutes == 1) s = "";
+        $(`#${this.counterElementId}`).text(
+          `${minutes} minute${s}, ${seconds} seconds ago`
+        );
+      }
     }
   }
 }
