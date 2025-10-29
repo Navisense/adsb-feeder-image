8de982672810adfece3de65bc5868f462ae45652
Marc Lehmann <marc@navisense.de>
2025-10-29 17:53:26 +0100

    Improve mDNS advertisement script
    We get our own IP from a route get now, which gets us the IP of the
    device that would be used to reach the internet.

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/mdns-advertise.bash
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/mdns-advertise.bash
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Advertise this machine with an mDNS name.
+#
+# The name to advertise must be given as the only argument, and must be a domain
+# name ending in .local.
+
+if [ ! -f /opt/adsb/scripts/lib-common.bash ] ; then
+    echo "Missing /opt/adsb/scripts/lib-common.bash, unable to continue."
+    exit 1
+else
+    source /opt/adsb/scripts/lib-common.bash
+    rootcheck
+fi
+
 mdns_name="${1}"
 
-/usr/bin/avahi-publish-address -R "${mdns_name}" $(avahi-resolve -4 -n $(hostname).local > /dev/null && ip -json addr show dev wlan0 | jq -r '[.[0].addr_info[] | select(.family=="inet")][0].local')
+own_ip=$(ip --json route get 8.8.8.8 | jq -r '.[0].prefsrc')
+if [ -z "${own_ip}" ] || [ "${own_ip}" == "null" ] ; then
+    log_and_exit_sync 1 $0 "Unable to find our own IP."
+fi
+
+/usr/bin/avahi-publish-address -R "${mdns_name}" "${own_ip}"
