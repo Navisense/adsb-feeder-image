4ea8d486b96f22e5a11317badacd2974a3e9ef5a
Marc Lehmann <marc@navisense.de>
2025-10-29 19:52:14 +0100

    Handle mDNS during hotspot
    While the hotspot is active, finding the IP via route get won't work
    since the network is unreachable. In that case, check if we have an IP
    that's the same as the hotspot IP. We can assume we're in hotspot mode
    then and advertise that.

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/mdns-advertise.bash
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/mdns-advertise.bash
@@ -17,6 +17,15 @@ mdns_name="${1}"
 
 own_ip=$(ip --json route get 8.8.8.8 | jq -r '.[0].prefsrc')
 if [ -z "${own_ip}" ] || [ "${own_ip}" == "null" ] ; then
+    log $0 "Unable to find our own IP through a route to the public internet."
+    # We may be in hotspot mode, where our IP will be 192.168.199.1.
+    own_ip=$(ip --json route show \
+        | jq -r '.[].prefsrc | select (.=="192.168.199.1")')
+    if [ -n "${own_ip}" ] ; then
+        log $0 "Found our own IP, we appear to be in hotspot mode."
+    fi
+fi
+if [ -z "${own_ip}" ] || [ "${own_ip}" == "null" ] ; then
     log_and_exit_sync 1 $0 "Unable to find our own IP."
 fi
 
