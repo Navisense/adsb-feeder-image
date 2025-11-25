cd638e2b006f0feaff317b20ea8639bb3fde9363
Marc Lehmann <marc@navisense.de>
2025-11-25 11:03:18 +0100

    Only publish service
    As it turns out, avahi-publish-service will actually also publish an
    address for the service (adding .local). It even handles the whole
    finding-our-own-IP business, making the script a whole lot easier.

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/mdns-advertise.bash
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/scripts/mdns-advertise.bash
@@ -11,30 +11,8 @@
 # has to be restarted. This is just a low-tech way of ensuring that the correct
 # IP is advertised eventually in case it changes.
 
-if [ ! -f /opt/adsb/scripts/lib-common.bash ] ; then
-    echo "Missing /opt/adsb/scripts/lib-common.bash, unable to continue."
-    exit 1
-else
-    source /opt/adsb/scripts/lib-common.bash
-    rootcheck
-fi
-
 mdns_hostname="${1}"
 
-own_ip=$(ip --json route get 8.8.8.8 | jq -r '.[0].prefsrc')
-if [ -z "${own_ip}" ] || [ "${own_ip}" == "null" ] ; then
-    log $0 "Unable to find our own IP through a route to the public internet."
-    # We may be in hotspot mode, where our IP will be 192.168.199.1.
-    own_ip=$(ip --json route show \
-        | jq -r '.[].prefsrc | select (.=="192.168.199.1")')
-    if [ -n "${own_ip}" ] ; then
-        log $0 "Found our own IP, we appear to be in hotspot mode."
-    fi
-fi
-if [ -z "${own_ip}" ] || [ "${own_ip}" == "null" ] ; then
-    log_and_exit_sync 1 $0 "Unable to find our own IP."
-fi
-
-avahi-publish-address -R "${mdns_hostname}.local" "${own_ip}" &
-avahi-publish-service "${mdns_hostname}" _http._tcp ${AF_WEBPORT:-80} &
-sleep 60
+# avahi-publish-service will publish a service, and also publish the address as
+# <service_name>.local.
+timeout 60 avahi-publish-service "${mdns_hostname}" _http._tcp ${AF_WEBPORT:-80}
