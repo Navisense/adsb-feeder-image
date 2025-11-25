a06418f9ee6966f944dec2cbb7b7d475aa413953
Marc Lehmann <marc@navisense.de>
2025-11-25 13:42:06 +0100

    Add feeder discovery service

--- src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/net.py
+++ src/modules/adsb-feeder/filesystem/root/opt/adsb/adsb-setup/net.py
@@ -1,6 +1,9 @@
 import json
 import logging
+import re
 import urllib.request
+
+import zeroconf as zc
 
 import util
 
@@ -41,3 +44,63 @@ def gitlab_repo() -> GitlabRepo:
     if _gitlab_repo is None:
         _gitlab_repo = GitlabRepo()
     return _gitlab_repo
+
+
+class FeederDiscoverer(zc.ServiceListener):
+    """
+    Service discovering other feeder devices.
+
+    Uses zeroconf to discover services advertised on the network that look like
+    Porttracker SDR services (i.e. <xxxxxxxx>-porttracker-sdr). Provides the
+    other_feeder_names property, which contains the names of all other feeders
+    that have been discovered.
+    """
+    _service_name_regex = re.compile(
+        r'(?P<hostname>[0-9a-fA-F]{8}-porttracker-sdr)\._http\._tcp\.local\.')
+
+    def __init__(self, own_feeder_name):
+        """
+        :param own_feeder_name: The name of this feeder. Will be excluded from
+            the other_feeder_names set.
+        """
+        self._logger = logging.getLogger(type(self).__name__)
+        self._own_feeder_name = own_feeder_name
+        self._zeroconf = zc.Zeroconf()
+        self._service_browser = zc.ServiceBrowser(
+            self._zeroconf, "_http._tcp.local.", self)
+        self._other_feeder_names = set()
+
+    def start(self):
+        # Zeroconf starts on construction.
+        assert self._zeroconf.started
+
+    def stop(self):
+        self._service_browser.cancel()
+        self._zeroconf.close()
+
+    @property
+    def other_feeder_names(self) -> frozenset[str]:
+        return frozenset(self._other_feeder_names)
+
+    def update_service(self, zc: zc.Zeroconf, type_: str, name: str) -> None:
+        pass
+
+    def remove_service(self, zc: zc.Zeroconf, type_: str, name: str) -> None:
+        if (other_feeder_name := self._extract_other_feeder_name(name)):
+            self._logger.info(
+                f"Other feeder {other_feeder_name} has gone away.")
+            self._other_feeder_names.discard(other_feeder_name)
+
+    def add_service(self, zc: zc.Zeroconf, type_: str, name: str) -> None:
+        if (other_feeder_name := self._extract_other_feeder_name(name)):
+            self._logger.info(f"Discovered other feeder {other_feeder_name}.")
+            self._other_feeder_names.add(other_feeder_name)
+
+    def _extract_other_feeder_name(self, service_name):
+        match = self._service_name_regex.match(service_name)
+        if not match:
+            return None
+        feeder_name = match.group("hostname")
+        if feeder_name == self._own_feeder_name:
+            return None
+        return feeder_name
